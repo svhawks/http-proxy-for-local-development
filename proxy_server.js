f4a394264f986087295ef49275ae1df2f0a6080d
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2015-05-12 20:17:13 +0300

    Added SSL keys and HTTP forwarding.

--- proxy_server.js
+++ proxy_server.js
@@ -1,15 +1,23 @@
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
 var https = require('https');
 
 var httpProxy = require('http-proxy');
 var _ = require('lodash');
 
+/*
+  Configuration
+*/
+
+// Default target that the proxy will forward requests to
 var defaultTarget = {
   host: '127.0.0.1',
   port: 3000
 };
 
+// If the request host matches one of these, the request will be forwarded to the specified target
+// You can also override the host like: {host: '192.168.0.28', port: 80}
 var customTargets = {
   'localmovielala.com':               {port: 3000},
   'api.localmovielala.com':           {port: 4000},
@@ -21,22 +29,55 @@ var customTargets = {
   'embedadmin.movielala.com':         {port: 5001}
 };
 
-var options = {
+// SSL key and certificates for HTTPS
+var httpsOptions = {
   key: fs.readFileSync(path.resolve(__dirname, './localmovielala.com.key'), 'utf-8'),
   cert: fs.readFileSync(path.resolve(__dirname, './localmovielala.com.cert'), 'utf-8')
 };
 
-var proxy = httpProxy.createProxyServer({});
+/*
+  Helpers
+*/
 
-https.createServer(options, function (req, res) {
+var requestListener = function requestListener(req, res) {
   
   var target = _.assign({},
     defaultTarget,
     customTargets[req.headers.host] || {}
   );
 
-  proxy.web(req, res, {
+  proxyServer.web(req, res, {
     target: target
   });
 
-}).listen(443);
+}
+
+var errorReporter = function errorReporter(thrower, err) {
+
+  console.log('Error in ' + thrower);
+  console.log(err);
+
+};
+
+/*
+  Proxy Server
+*/
+
+var proxyServer = httpProxy.createProxyServer({})
+  .on('error', _.bind(errorReporter, null, 'proxy'));
+
+/*
+  HTTPS Server
+*/
+
+https.createServer(httpsOptions, requestListener)
+  .listen(443)
+  .on('error', _.bind(errorReporter, null, 'https'));
+
+/*
+  HTTP Server
+*/
+
+http.createServer(requestListener)
+  .listen(443)
+  .on('error', _.bind(errorReporter, null, 'http'));
